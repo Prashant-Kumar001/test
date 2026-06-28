d5a42848de9fd03e3adc30a8432738dd957c9883
Prashant <prashant0kumar101@gmail.com>
2026-06-28 16:04:44 +0530

    fix: add required field to author and implement soft delete feature

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -15,11 +15,25 @@ const postSchema = new mongoose.Schema(
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+    },
+
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+
+    deletedAt: {
+      type: Date,
+      default: null,
     },
   },
   {
     timestamps: true,
   }
 );
+
+// Add index for soft delete queries
+postSchema.index({ isDeleted: 1 });
 
 export default mongoose.model("Post", postSchema);
